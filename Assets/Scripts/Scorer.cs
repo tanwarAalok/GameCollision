e69b1ca1ef25b04220533a1422c6d2839c5372ae
Aalok Tanwar <aalok.singh@vitbhopal.ac.in>
2021-12-22 11:17:09 +0530

    first commit

---
+++ Assets/Scripts/Scorer.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scorer : MonoBehaviour
+{
+    public int score = 0;
+    private void OnCollisionEnter(Collision other) {
+        if(other.gameObject.tag != "Hit") score++;
+        Debug.Log("You've bumped "+ score + " times");
+    }
+    
+}
---
+++ Assets/Scripts/Spinner.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spinner : MonoBehaviour
+{
+
+    [SerializeField] float yVal = 0.7f;
+    void Update()
+    {
+        transform.Rotate(0, yVal, 0);
+    }
+}
